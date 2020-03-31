b03075eb4d60a3571ebb9f4827653ac55ce1be75
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-03-31 10:39:03 +0200

    Add tests

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
+
+def test_forms(cldf_dataset):
+    assert len(list(cldf_dataset["FormTable"])) == 2354 
+    assert any(f["Form"] == "huniɽi" for f in cldf_dataset["FormTable"])
+
+def test_parameters(cldf_dataset):
+    assert len(list(cldf_dataset["ParameterTable"])) == 243
+
+
+def test_languages(cldf_dataset):
+    assert len(list(cldf_dataset["LanguageTable"])) == 14
+
